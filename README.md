7a16dde294be6d8f46114439e812ca9d28d5cde5
Brian Harvey <harveyb4b@yahoo.com>
2020-07-20 22:15:43 -0400

    Updating my portfolio showcase

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# PortfolioShowcase-1
+# The Harvey Random Password Generator
+**Version 1.0.0**
+Stop creating your own password 
+
+The Random Password Generator will help you save time by generating you a password based on your specific. 
+<img src="lib\readmeImages\RandomPasswordGenerator.PNG">
+
+If you are interested in creating a password select what parameters you would like to add 
+<img src="lib\readmeImages\RandomPasswordGeneratorParameters.PNG"">
+
++UpperCase
++LowerCase
++Numbers
++Symbols
+
+You will also be able to select your password length between 8 - 128 characters for ultimate security 
+
+<br>
+
+
+## Why should you use the Harvey Random Password Generator
+Stay Secure and generate your random password today
+
+
+## Features
+Random Password Generator will provide a unique and secure password based on your criteria
+
+Based on your criteria this app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code.
+
+If your password requires any or all of these criteria this is the right app for you!
+
+
+## Created by
+Brian Harvey
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,89 @@
+//DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboard = document.getElementById('clipboard');
+
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
+}
+// //Add clipboardfunction for ease of use
+// Clipboard.addEventListener('click', () => {
+// 	const textarea = document.createElement('textarea');
+// 	const password = resultEl.innerText;
+	
+// 	if(!password) { return; }
+	
+// 	textarea.value = password;
+// 	document.body.appendChild(textarea);
+// 	textarea.select();
+// 	document.execCommand('copy');
+// 	textarea.remove();
+// 	alert('Password copied to clipboard');
+// });
+//Generate Event Listener
+generate.addEventListener('click', () => {
+	const length = +lengthEl.value;
+	const hasLower = lowercaseEl.checked;
+	const hasUpper = uppercaseEl.checked;
+	const hasNumber = numbersEl.checked;
+	const hasSymbol = symbolsEl.checked;
+	
+	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+//password function
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatedPassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	//console.log('typesCount: ', typesCount);
+    //console.log('typesArr: ', typesArr);
+
+    // Doesn't have a selected type
+	if(typesCount === 0) {
+		return '';
+	}
+	
+	// create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	 //console.log(generatepassword.slice(0, length)); 
+	const finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+}
+//generator functions
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+
+
+
+
+
